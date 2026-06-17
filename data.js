c84a4327e8a4e7f09c7b7c8a0f07effd6f66c0a5
almehedi0304 <jhpi133826@gmail.com>
2026-06-17 10:32:02 +0600

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,42 @@
 const family = [
   {
     id: 1,
-    name: "মেহেদী",
+    name: "দাদা",
     gender: "male",
     father: null,
-    mother: null
+    mother: null,
+    image: "https://via.placeholder.com/100"
   },
   {
     id: 2,
-    name: "সাওদা",
+    name: "দাদী",
     gender: "female",
     father: null,
-    mother: null
+    mother: null,
+    image: "https://via.placeholder.com/100"
   },
   {
     id: 3,
-    name: "সায়ন",
+    name: "মেহেদী (বাবা)",
     gender: "male",
     father: 1,
-    mother: 2
+    mother: 2,
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    id: 4,
+    name: "সাওদা (মা)",
+    gender: "female",
+    father: null,
+    mother: null,
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    id: 5,
+    name: "সায়ন",
+    gender: "male",
+    father: 3,
+    mother: 4,
+    image: "https://via.placeholder.com/100"
   }
 ];
