d0eef7bda24e6534fcba07d2f231970e698f88db
almehedi03042000 <almehedi0304@gmail.com>
2026-07-18 11:34:20 +0600

    Update app.js version 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,84 @@
+/* ==========================================
+   Sardar Family Tree
+   Version 0.1
+========================================== */
 
+"use strict";
+
+// =============================
+// Project Information
+// =============================
+
+const APP = {
+    name: "Sardar Family Tree",
+    version: "0.1"
+};
+
+console.log(`${APP.name} v${APP.version} Loaded`);
+
+// =============================
+// DOM Elements
+// =============================
+
+const treeContainer = document.getElementById("treeContainer");
+const searchBox = document.getElementById("searchBox");
+
+const zoomInBtn = document.getElementById("zoomIn");
+const zoomOutBtn = document.getElementById("zoomOut");
+const resetZoomBtn = document.getElementById("resetZoom");
+
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+// =============================
+// Placeholder Functions
+// =============================
+
+function loadFamilyData() {
+    console.log("Family Database will load here...");
+}
+
+function renderTree() {
+    console.log("Tree Rendering...");
+}
+
+function searchMember() {
+    console.log("Searching...");
+}
+
+function zoomIn() {
+    console.log("Zoom In");
+}
+
+function zoomOut() {
+    console.log("Zoom Out");
+}
+
+function resetZoom() {
+    console.log("Reset Zoom");
+}
+
+function toggleDarkMode() {
+    console.log("Dark Mode");
+}
+
+// =============================
+// Events
+// =============================
+
+zoomInBtn.addEventListener("click", zoomIn);
+
+zoomOutBtn.addEventListener("click", zoomOut);
+
+resetZoomBtn.addEventListener("click", resetZoom);
+
+darkModeBtn.addEventListener("click", toggleDarkMode);
+
+searchBox.addEventListener("input", searchMember);
+
+// =============================
+// Start Application
+// =============================
+
+loadFamilyData();
+
+renderTree();
