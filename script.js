5aebdd221602f4b972f1c34a0e1843c505c4471d
almehedi03042000 <almehedi0304@gmail.com>
2026-08-27 12:01:31 +0600

    Update profile photo URL in script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const familyData = {
     idNo: "S-001", dob: "অজানা", birthPlace: "অজানা", currentAddress: "অজানা",
     occupation: "কৃষি", education: "অশিক্ষিত", mobile: "N/A", bloodGroup: "N/A",
     maritalStatus: "বিবাহিত", spouse: null, father: null, mother: null, specialNote: "মূল পূর্বপুরুষ",
-    profilePhoto: "", gallery: [],
+    profilePhoto: "https://ibb.co/S760bKct", gallery: [],
     children: [
         {
             id: "akali", nameBn: "আকালী সর্দার", nameEn: "Akali Sardar", gender: "male", isDeceased: false,
